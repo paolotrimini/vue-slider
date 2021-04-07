4afe56384844ed43120a2aa0874b820266e97b01
paolotrimini <paolo.trimini@gmail.com>
2021-04-07 17:48:59 +0200

    slider + bonus

--- js/script.js
+++ js/script.js
@@ -8,31 +8,53 @@ function initVue() {
 
         el: '#app',
         data: {
+            img_corr : 0,
+            timer: 0,
+            imgs : [
 
+                'img/ny.jpg',
+                'img/la.jpg',
+                'img/tk.jpg',
+                'img/rm.jpg'
+            ],
         },
 
         methods: {
 
             // img successiva
-
+            next() {
+                this.img_corr += 1;
+                if(this.img_corr == this.imgs.length) {
+                    this.img_corr = 0;
+                }
+            },
 
             // img precedente
-
+            prev() {
+                this.img_corr -= 1;
+                if(this.img_corr < 0) {
+                    this.img_corr = this.imgs.length -1;
+                }
+            },
 
             // BONUS: autoplay 3 secondi
-
+            play() {
+                let app = this;
+                this.timer = setInterval(function() {
+                    app.next();
+                }, 3000);}
 
         },
 
-             // funzione autoplay
-
+        // autoplay
+        created: function() {
+            this.play();
+        }
 
     });
 
 
 }
-
-
 
 function init() {
     initVue();
