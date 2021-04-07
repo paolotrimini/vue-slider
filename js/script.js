62c9a317251e5631dce5641ba938b41d99c69e94
paolotrimini <paolo.trimini@gmail.com>
2021-04-07 14:36:33 +0200

    caricamento file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Ricreare lo slider visto in jquery, con tecnologia Vue.
+// BONUS: aggiungere un timer che permetta di ciclare le immagini
+// in automatico (cambio ogni 3sec).
+
+function initVue() {
+    new Vue({
+        el: '#app',
+        data: {
+
+        },
+        methods: {
+
+        }
+    });
+}
+
+
+
+function init() {
+    initVue();
+}
+// $(init);
+document.addEventListener('DOMContentLoaded', init);
